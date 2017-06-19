7065dad2035bef9ab0af077ae08a7b91a8355837
Yulong Tan <yulong@MSEAVVYTAN.local>
2017-06-19 15:03:53 -0700

    initial

---
+++ TipCalculator/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TipCalculator {
+    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+
+	public class MainActivity : Activity {
+
+        // Fields
+        private EditText inputBill;
+        private Button calculateButton;
+        private TextView outputTip, outputTotal;
+
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
